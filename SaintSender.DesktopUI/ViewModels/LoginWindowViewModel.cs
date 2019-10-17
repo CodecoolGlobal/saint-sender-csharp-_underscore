3b98981d2e892468136cb6a9cb3854320e21ddcc
KalloDavid93 <43600176+KalloDavid93@users.noreply.github.com>
2019-10-17 15:45:50 +0200

    Update LoginWindowViewModel.cs

--- SaintSender.DesktopUI/ViewModels/LoginWindowViewModel.cs
+++ SaintSender.DesktopUI/ViewModels/LoginWindowViewModel.cs
@@ -15,17 +15,6 @@ namespace SaintSender.DesktopUI.ViewModels
         public string userName { get; set; }
         public string password { get; set; }
 
-
-        public static bool ValidEmailAdress(string email)
-        {
-            if(!Regex.IsMatch(email, @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,3}$"))
-            {
-                MessageBox.Show("Email address is invalid!");
-                return false;
-            }
-            return true;
-        }
-
         public void ExecuteLogin()
         {
             LoginService.Login(userName, password);
