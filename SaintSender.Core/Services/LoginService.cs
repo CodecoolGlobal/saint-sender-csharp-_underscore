8d9f24d46ebdf09a729265c620721694de664cd7
KalloDavid93 <43600176+KalloDavid93@users.noreply.github.com>
2019-10-17 15:45:14 +0200

    Update LoginService.cs

--- SaintSender.Core/Services/LoginService.cs
+++ SaintSender.Core/Services/LoginService.cs
@@ -10,18 +10,12 @@ namespace SaintSender.Core.Services
 {
     public class LoginService
     {
-
-        //string pw = "wjurtaqxhvfupaal";
-        //string email = "undescoretestemail@gmail.com";
-
         public static void Login(string userName, string password)
         {
             ImapClient client = new ImapClient();
             client.Connect("imap.gmail.com", 993, true);
             client.AuthenticationMechanisms.Remove("XOAUTH2");
             client.Authenticate(userName, password);
-        }
-
-        
+        } 
     }
 }
